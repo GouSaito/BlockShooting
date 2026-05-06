b6d09cc1fdc392bda21f026ceabee845d14fe2c0
GouSaito <tys1029@gmail.com>
2026-05-07 06:37:31 +0900

    Initial Commit

---
+++ BlockShooting/BlockShootingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockShootingApp.swift
+//  BlockShooting
+//
+//  Created by 斎藤剛 on 2026/05/07.
+//
+
+import SwiftUI
+
+@main
+struct BlockShootingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
